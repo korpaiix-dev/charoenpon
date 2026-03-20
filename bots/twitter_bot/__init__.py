14872eac46c2c1de878a18100ad1548347651c82
korpaiix-dev <korpaiix-dev@users.noreply.github.com>
2026-03-20 11:51:42 +0000

    feat: Twitter/X Bot auto post 4 รอบ/วัน + teaser เบลอ + hashtags

---
+++ bots/twitter_bot/__init__.py
@@ -0,0 +1,1 @@
+# Twitter Bot - Auto Post module
---
+++ bots/twitter_bot/__main__.py
@@ -0,0 +1,6 @@
+"""Run Twitter Bot scheduler: python -m bots.twitter_bot"""
+
+from bots.twitter_bot.scheduler import main
+
+if __name__ == "__main__":
+    main()
